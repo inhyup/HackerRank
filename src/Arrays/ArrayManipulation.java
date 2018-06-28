b45bd298b66f6d63e6cad3d8e6be9517eaa8b64a
Inhyup <hyup3569@gmail.com>
2018-06-28 17:46:15 -0600

    array

---
+++ src/Arrays/ArrayManipulation.java
@@ -0,0 +1,65 @@
+package Arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArrayManipulation {
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+    	long ans = 0l;
+    	long[] a = new long[n];
+    	
+    	for (int j = 0; j < queries.length; j++) {
+    		for (int i = queries[j][0]-1; i <= queries[j][1]-1; i++) {
+    		
+    			
+        		a[i] += queries[j][2];
+        	}
+        		
+    	}
+    	
+    	for (int i = 0; i < a.length; i++) {
+    		ans = Math.max(ans, a[i]);
+    	}
+
+    	return ans;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
